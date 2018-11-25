4af8a797d0c8971936b2d84a1cc6b11a2d37d2b3
no name <ginobilinie@users.noreply.github.com>
2018-11-25 01:37:51 -0500

    Update threeSumClosest.cpp

--- threeSumClosest.cpp
+++ threeSumClosest.cpp
@@ -23,3 +23,32 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int len = nums.size();
+        int minMargin = INT_MAX;
+        int res = target;
+        sort(nums.begin(),nums.end());
+        for (int i=0; i<len-2; i++)//fix a value
+        {
+            int left = i+1;
+            int right = len-1;
+            while(left<right)
+            {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (abs(sum - target)<minMargin)
+                {
+                    minMargin = abs(sum - target);
+                    res = sum;     
+                }
+                if (sum<target)
+                    left++;
+                else
+                    right--;
+            }
+        }
+        return res;
+    }
+};
