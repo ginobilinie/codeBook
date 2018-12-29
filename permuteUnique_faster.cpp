df18899ea35767860594e12a3b1371ea92ac1bc8
no name <ginobilinie@users.noreply.github.com>
2018-12-29 00:48:57 -0500

    Create permuteUnique_faster.cpp

---
+++ permuteUnique_faster.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+ 
+    // set<vector<int>> myset;
+    vector<vector<int>> res;
+    vector<bool> used;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<int> currNums;
+        vector<vector<int>> rr;
+        if (nums.empty())
+        {
+            // rr.push_back(currNums);
+            return rr;
+        }
+        sort(nums.begin(),nums.end());
+        for (int i=0; i<nums.size(); i++)
+            used.push_back(0);
+        // for (int i=0; i<nums.size(); i++)
+        // {
+        //     currNums.push_back(nums[i]);
+        //     used[i] = 1;
+        //     dfs(nums,currNums);
+        //     currNums.clear();
+        //     used[i] = 0;
+        // }
+        dfs(nums,currNums);
+        // vector<vector<int>> res(myset.begin(),myset.end());
+        return res;
+    }
+    
+    //k indicates the current size for currNums
+    void dfs(vector<int>& nums, vector<int>& currNums)
+    {
+        if (currNums.size()==nums.size())
+        {
+            res.push_back(currNums);
+            return;
+        }
+        vector<int> tmp;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (!used[i])
+            {
+                used[i] = 1;
+                tmp = currNums;
+                currNums.push_back(nums[i]);
+                dfs(nums, currNums);
+                used[i] = 0;
+                currNums.pop_back();
+                // currNums.erase(currNums.end());
+                while (i<nums.size()-1 && nums[i]==nums[i+1]) i++;//this is used to avoid repeat
+            }
+        }
+    }
+};
