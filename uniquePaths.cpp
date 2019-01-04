b95b204e2ff20f3a5fecae72e6adc67ac1921d0c
no name <ginobilinie@users.noreply.github.com>
2019-01-03 23:01:48 -0500

    Update uniquePaths.cpp

--- uniquePaths.cpp
+++ uniquePaths.cpp
@@ -32,3 +32,20 @@ public:
         return res;
     }
 };
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        int dp[100][100];
+        for (int i=0; i<m; i++)
+            dp[0][i] = 1;
+        for (int j=0; j<n; j++)
+            dp[j][0] = 1;
+        for (int i=1; i<n; i++)
+            for (int j=1; j<m; j++)
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        int res = dp[n-1][m-1];
+        return res;
+    }
+};
