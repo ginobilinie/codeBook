76b19f4749b528cb99af94aa0b5092fa26178a67
no name <ginobilinie@users.noreply.github.com>
2019-01-07 18:28:11 -0500

    biSearch

--- biSearchRotate.cpp
+++ biSearchRotate.cpp
@@ -12,13 +12,19 @@ public:
         bool res = 0;
         while (l<=r)
         {
+            //the key point here is that we have duplicates in the array,
+            //especially the cases the duplicates appear in the start and end
+            //we should first process one end, then we can really decide which part
+            //is the increasing part
+            while(l<r&&nums[l]==nums[r])
+                l++;
             mid = (l+r)/2;
             if (nums[mid]==target)
             {
                 res = 1;
                 break;
             }
-            else if (nums[mid]<nums[r])//right side is ordered
+            else if (nums[mid]<=nums[r])//right side is ordered
             {
                 if (target>=nums[mid] && target<=nums[r])
                 {
@@ -29,7 +35,7 @@ public:
                     r = mid - 1;
                 }
             }
-            else //left side is ordered
+            else
             {
                 if (nums[l]<=target&&target<=nums[mid])
                 {
