9f8cea7bc3a8fbe5870477061a0c885ba54054f3
no name <ginobilinie@users.noreply.github.com>
2019-01-11 16:34:27 -0500

    arrayScan

---
+++ maxProfit.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty())
+            return 0;
+        int maxVal,  minPrice=INT_MAX;
+        for (int i=0; i<prices.size(); i++)
+        {
+            minPrice = minPrice<prices[i]?minPrice:prices[i];
+            int profit = prices[i] - minPrice;
+            maxVal = maxVal>profit?maxVal:profit;
+        }
+        return maxVal;
+        // return 0;
+    }
+};
