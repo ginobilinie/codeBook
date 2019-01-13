d06b06e0cb0170597685ab4f4e1e36e5ae1c638e
no name <ginobilinie@users.noreply.github.com>
2019-01-12 23:43:56 -0500

    bfs

--- bfs/ladderLength.cpp
+++ bfs/ladderLength.cpp
@@ -12,16 +12,25 @@ public:
         int depth = 0;
         bool flag = 0;
         vector<bool> used(wordList.size(),0);
+        queue<int> levelq;
+        levelq.push(1);
+        for (int i=0; i<wordList.size(); i++)
+            if (beginWord==wordList[i])
+                used[i] = 1;
         
         while(!myq.empty())
         {
             if (myq.front()==endWord)
             {
                 flag = 1;
+                depth = levelq.front();
+               //depth++;
                 break;
             }
             string word = myq.front();
             myq.pop();
+            int level = levelq.front();
+            levelq.pop();
             //transform
             for (int i=0; i<wordList.size(); i++)
             {
@@ -29,16 +38,17 @@ public:
                 if (!used[i]&&diffNums(word,ww)==1)
                 {
                     myq.push(ww);
+                    levelq.push(level+1);
                     used[i] = 1;
                 }
             }
             
-            depth++;
+            depth = level;
         }
         
         if (!flag)
             return 0;
-        return depth-1;
+        return depth;
     }
     int diffNums(string s1, string s2)
     {
