a86e3884fd7cd8a0ceddb0c49ad646975d6dc78b
no name <ginobilinie@users.noreply.github.com>
2019-01-16 18:21:02 -0500

    recursive

---
+++ tree/postorderTraversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> vec;
+        postorder(root,vec);
+        return vec;
+    }
+    
+    void postorder(TreeNode* root, vector<int>& vec)
+    {
+        if (!root)
+            return;
+        postorder(root->left, vec);
+        postorder(root->right, vec);
+        vec.push_back(root->val);
+    }
+};
