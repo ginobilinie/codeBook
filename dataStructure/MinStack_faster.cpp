4e97eca15da64cd27fedc2831c28cfdced25d63a
no name <ginobilinie@users.noreply.github.com>
2019-01-18 14:26:55 -0500

    vector+map

---
+++ dataStructure/MinStack_faster.cpp
@@ -0,0 +1,63 @@
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        // minVal = INT_MAX;
+    }
+    
+    void push(int x) {
+        vec.push_back(x);
+        if (mymap.count(x))
+            mymap[x]++;
+        else
+            mymap[x] = 1;
+        // if (x<minVal);
+        //     minVal = x;
+    }
+    
+    void pop() {
+        //but we have to update he minVal
+        if (!vec.empty())
+        {
+            int kk = vec[vec.size()-1];
+            vec.pop_back();
+            if (mymap[kk]>1)
+                mymap[kk]--;
+            else
+                mymap.erase(kk);
+        }
+    }
+    
+    int top() {
+        if (!vec.empty())
+            return vec[vec.size()-1];
+        else
+            return -1;
+    }
+    
+    int getMin() {
+        //we can use for loop to get the smallest one, but it is then O(n)
+        //if we like to have O(1) for getMin, we need a priority queue
+        // int res = INT_MAX;
+        // for (int i=0; i<vec.size(); i++)
+        //     if (vec[i]<res)
+        //         res = vec[i];
+        auto it = mymap.begin();
+        int res = it->first;
+        return res;
+    }
+    
+private:
+    vector<int> vec;
+    map<int,int> mymap;
+    // int minVal;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
