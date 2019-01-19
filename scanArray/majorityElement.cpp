23f57ac7a9e6aac24cb25ee82bab528c7d7fddf5
no name <ginobilinie@users.noreply.github.com>
2019-01-19 17:17:41 -0500

    Update majorityElement.cpp

--- scanArray/majorityElement.cpp
+++ scanArray/majorityElement.cpp
@@ -21,3 +21,34 @@ public:
         return res;
     }
 };
+
+//only one for loop pass
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int> mymap;
+        int maxVal = INT_MIN, res = -1;
+        for (int i=0; i<nums.size(); i++)
+        {
+            if (mymap.count(nums[i]))
+                mymap[nums[i]]++;
+            else
+                mymap[nums[i]] = 1;
+            if (maxVal<mymap[nums[i]])
+            {
+                maxVal = mymap[nums[i]];
+                res = nums[i];
+            }
+        }
+        
+        // for (auto it: mymap)
+        // {
+        //     if (it.second>maxVal)
+        //     {
+        //         maxVal = it.second;
+        //         res = it.first;
+        //     }
+        // }
+        return res;
+    }
+};
