d98add3412ef6f06e842a89c4be591c9bceb0a75
no name <ginobilinie@users.noreply.github.com>
2019-01-19 20:45:10 -0500

    inorder+vector

---
+++ tree/BSTIterator.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        currNode = root;
+        inorder(root);
+        currInd = 0;
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        if (hasNext()==0)
+            return -1;
+        return myvec[currInd++];
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+       if (currInd<myvec.size())
+            return true;
+        else
+            return false;
+    }
+    void inorder(TreeNode* root)
+    {
+        if (!root)
+            return;
+        inorder(root->left);
+        myvec.push_back(root->val);
+        inorder(root->right);
+    }
+private:
+    TreeNode* currNode;
+    vector<int> myvec;
+    int currInd;
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
