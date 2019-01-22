1911dda0f3b0511fa1896da58a25626191cf76f4
no name <ginobilinie@users.noreply.github.com>
2019-01-22 02:09:39 -0500

    Update tallestBillboard.cpp

--- dfs/tallestBillboard.cpp
+++ dfs/tallestBillboard.cpp
@@ -8,13 +8,54 @@ public:
             return 0;
         int sum = 0;
         for (int i=0; i<rods.size(); i++)
-            sum += sum[i];
+            sum += rods[i];
         sum = sum/2+1;
-        
+        int res = 0;
         while (sum--)
         {
+            cout<<sum<<endl;
             vector<bool> used(rods.size(),0);
-            w
+            bool flag1 = 0, flag2=0;
+            dfs(rods, sum,0, used, flag1);
+            if (!flag1)
+                continue;
+            cout<<"haha0: ";
+            for (int i=0; i<rods.size(); i++)
+                cout<<used[i]<<" ";
+            cout<<" haha1"<<endl;
+            dfs(rods,sum,0,used,flag2);
+            cout<<"haha2: ";
+            for (int i=0; i<rods.size(); i++)
+                cout<<used[i]<<" ";
+            cout<<" haha3"<<endl;
+            if (flag2)
+            {
+                res = sum;
+                break;
+            }
+        }
+        return res;
+    }
+    
+    void dfs(vector<int>& rods, int target, int currSum, vector<bool>& used, bool& flag)
+    {
+        if (currSum==target)
+        {
+            flag = 1;
+            return;
+        }
+        for (int i=0; i<rods.size(); i++)
+        {
+            if (!used[i])
+            {
+                if (currSum+rods[i]>target)
+                    continue;
+                used[i] = 1;
+                dfs(rods,target,currSum+rods[i],used,flag);
+                if (flag)
+                    return;
+                used[i] = 0;
+            }
         }
     }
 };
