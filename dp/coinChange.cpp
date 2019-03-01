c0859b417a83d433087d31088a1fd79c5ac603f6
no name <ginobilinie@users.noreply.github.com>
2019-03-01 10:32:23 -0500

    Update coinChange.cpp

--- dp/coinChange.cpp
+++ dp/coinChange.cpp
@@ -3,17 +3,21 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         if (amount==0)
             return 0;
-        vector<int> dp(amount+1,0);
-        
+        if (amount<0)
+            return -1;
+        vector<int> dp(amount+1,INT_MAX);
+        dp[0] = 0;
         for (int i=1; i<amount+1; i++)
         {
             for (int coin:coins)
             {
-                if (i-coin>0)
-                    dp[i] = min(dp[i-coin],dp[i])+1;
+                if (i-coin>=0)
+                    dp[i] = min(dp[i-coin],dp[i]);
             }
+            if (dp[i]!=INT_MAX)
+                dp[i]++;
         }
-        if (dp[amount]==0)
+        if (dp[amount]==INT_MAX)
             return -1;
         return dp[amount];
     }
